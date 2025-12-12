1cbdcc0d1a6f8ab1ba1dc861b4a7b4adb5bbdb45
isylsam <isylvestersam@gmail.com>
2025-12-12 01:23:33 +0100

    goodnight

--- src/store/data.js
+++ src/store/data.js
@@ -3,7 +3,12 @@ import cabbageImage from '../Images/cabbage.png';
 import cornImage from '../Images/corn.png';
 import greenPepperImage from '../Images/green-pepper.png';
 import lettuceImage from '../Images/lettucee.png';
-
+import indianMaltaImage from '../Images/indian-malta.png';
+import eggplantImage from '../Images/eggplant.png';
+import redCapsicumImage from '../Images/capsicum.png';
+import redTomatoImage from '../Images/tomato.png';
+import bigPotatoImage from '../Images/potato.png';
+import cauliflowerImage from '../Images/cauliflower.png';
 
 
 export const featuredProducts = [
@@ -42,3 +47,76 @@ export const featuredProducts = [
     rating: 4.3
   }
 ]
+
+export const hotDeals = [
+  {
+    id: 1,
+    name: "Green Apple ",
+    price: 1.99,
+    image: appleImage,
+    rating: 4.5
+  },
+  {
+    id: 2,
+    name: "Indian Malta",
+    price: 0.99,
+    image: indianMaltaImage,
+    rating: 4.5
+  },
+  {
+    id: 3,
+    name: "Green Lettuce",
+    price: 1.49,
+    image: lettuceImage,
+    rating: 4.0
+  }
+]
+
+export const bestSellers = [
+  {
+    id: 1,
+    name: "Eggplant", 
+    price: 1.99,
+    image: eggplantImage,
+    rating: 4.5
+  },
+  {
+    id: 2,
+    name: "Red Capsicum",
+    price: 0.99,
+    image: redCapsicumImage,
+    rating: 4.5
+  },
+  {
+    id: 3,
+    name: "Red Tomato",
+    price: 1.49,
+    image: redTomatoImage,
+    rating: 4.0
+  }
+]
+
+export const topRated = [ 
+  {
+    id: 1,
+    name: "Big Potato",
+    price: 1.99,
+    image: bigPotatoImage,
+    rating: 4.5
+  },
+  {
+    id: 2,
+    name: "Corn",
+    price: 0.99,
+    image: cornImage,
+    rating: 4.5
+  },
+  {
+    id: 3,
+    name: "Fresh Cauliflower",
+    price: 1.49,
+    image: cauliflowerImage,
+    rating: 4.0
+  }
+] 
+
